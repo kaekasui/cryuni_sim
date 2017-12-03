e73b07c2d2db90080eeaeefcab004dce9e56817d
kaekasui <kaekasui_github@icloud.com>
2017-12-03 19:27:41 +0900

    Modify following rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,10 +17,10 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 include ActionDispatch::TestProcess
 
 Shoulda::Matchers.configure do |config|
- config.integrate do |with|
-   with.test_framework :rspec
-   with.library :rails
- end
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
 
 Capybara.default_max_wait_time = 20
