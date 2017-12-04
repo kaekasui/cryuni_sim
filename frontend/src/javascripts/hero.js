2c5b3aa8eabaa02879a6e5e76a86848790b37e89
kaekasui <kaekasui_github@icloud.com>
2017-12-04 15:34:39 +0900

    Add the event on click hero image icon

--- frontend/src/javascripts/hero.js
+++ frontend/src/javascripts/hero.js
@@ -4,13 +4,30 @@ import PropTypes from 'prop-types'
 export default class Hero extends React.Component {
   constructor(props) {
     super(props)
+    this.handleClickHeroImage = this.handleClickHeroImage.bind(this)
+  }
+
+  handleClickHeroImage() {
+    this.getHeroAbilities()
+  }
+
+  getHeroAbilities() {
+    fetch('api/heros/' + this.props.hero.id + '/hero_abilities')
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({heros: res})
+        console.log(res)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
   }
 
   render() {
     return (
       <div className='heroComponent'>
-        <span className='hero_image'>
-          <img src={ 'assets/' + this.props.hero.image_name } />
+        <span className='hero_image' onClick={this.handleClickHeroImage} >
+          <img src={'assets/' + this.props.hero.image_name} />
         </span>
       </div>
     )
