a9c8f6bdaabb67c2a0cf69a07e2b72f9c33dd50d
kaekasui <kaekasui_github@icloud.com>
2017-12-05 00:43:47 +0900

    Fix following rubocop

--- app/decorators/hero_ability_decorator.rb
+++ app/decorators/hero_ability_decorator.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class HeroAbilityDecorator < Draper::Decorator
   delegate_all
 
   def intimacy_level
     level =
       if intimacy_level_from == intimacy_level_to
-        "#{intimacy_level_from}"
+        intimacy_level_from.to_s
       else
         "#{intimacy_level_from}〜#{intimacy_level_to}"
       end
