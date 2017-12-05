d6f328ceccece52da270b0f2c5787f76428454ed
kaekasui <kaekasui_github@icloud.com>
2017-12-06 02:07:00 +0900

    Add the column to get the hero ability info

--- app/serializers/hero_ability_serializer.rb
+++ app/serializers/hero_ability_serializer.rb
@@ -4,6 +4,7 @@ class HeroAbilitySerializer < ActiveModel::Serializer
   attributes :id, :stage, :intimacy_level
 
   belongs_to :hero
+  has_many :attached_abilities
 
   def intimacy_level
     object.decorate.intimacy_level
