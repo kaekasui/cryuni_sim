fd7e05d5491601af2d4d8bd88002f67ea5ce5352
kaekasui <kaekasui_github@icloud.com>
2017-12-06 16:56:39 +0900

    Add the column data for the whole image name

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,8 @@ CSV.foreach('db/seeds/heros.csv') do |row|
   hero = Hero.find_or_initialize_by(name: row[0])
 
   hero.update_attributes(
-    image_name: row[1]
+    image_name: row[1],
+    whole_image_name: row[2]
   )
 end
 
