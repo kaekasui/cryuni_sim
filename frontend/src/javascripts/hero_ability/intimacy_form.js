5e1ebbd4d81e6a837b761c82fe968f3d08d7ad59
kaekasui <kaekasui_github@icloud.com>
2017-12-07 11:16:31 +0900

    Remove commentout

--- frontend/src/javascripts/hero_ability/intimacy_form.js
+++ frontend/src/javascripts/hero_ability/intimacy_form.js
@@ -4,29 +4,12 @@ import PropTypes from 'prop-types'
 export default class IntimacyForm extends React.Component {
   constructor(props) {
     super(props)
-    //this.handleClickHeroImage = this.handleClickHeroImage.bind(this)
     this.handleChangeIntimacy = this.handleChangeIntimacy.bind(this)
   }
 
   handleChangeIntimacy(e) {
-    //TODO: レベルを入力
     this.props.handleSetIntimacy(e.target.value)
   }
-  //handleClickHeroImage() {
-  //  this.getHeroAbilities()
-  //  this.props.handleLoadHero(this.props.hero)
-  //}
-
-  //getHeroAbilities() {
-  //  fetch('api/heros/' + this.props.hero.id + '/hero_abilities')
-  //    .then((res) => res.json())
-  //    .then((res) => {
-  //      this.props.handleLoadHeroAbilities(res)
-  //    })
-  //    .catch((error) => {
-  //      console.error(error)
-  //    })
-  //}
 
   render() {
     return (
@@ -39,8 +22,5 @@ export default class IntimacyForm extends React.Component {
 }
 
 IntimacyForm.propTypes = {
-  //hero: PropTypes.object.isRequired,
   handleSetIntimacy: PropTypes.func.isRequired
-  //handleLoadHero: PropTypes.func.isRequired,
-  //handleLoadHeroAbilities: PropTypes.func.isRequired
 }
