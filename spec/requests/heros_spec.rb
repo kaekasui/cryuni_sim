1a253232bb0060cd2f0f35a61877bb9d7c280620
kaekasui <kaekasui_github@icloud.com>
2017-12-22 14:44:31 +0900

    Fix the spec for locked column

--- spec/requests/heros_spec.rb
+++ spec/requests/heros_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 describe 'GET /api/heros', autodoc: true do
-  let!(:hero1) { create(:hero, name: '英雄1') }
-  let!(:hero2) { create(:hero, name: '英雄2') }
+  let!(:hero1) { create(:hero, name: '英雄1', locked: true) }
+  let!(:hero2) { create(:hero, name: '英雄2', locked: false) }
 
   it '200とデータが返ってくること' do
     get '/api/heros'
@@ -23,7 +23,7 @@ describe 'GET /api/heros', autodoc: true do
         name: '英雄2',
         image_name: 'claudia.jpg',
         whole_image_name: 'sd_claudia.png',
-        locked: true
+        locked: false
       }
     ]
     expect(response.body).to be_json_as(json)
