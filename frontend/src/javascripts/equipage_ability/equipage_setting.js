653f5393ae4850609a8c82007cb5766079b00d37
kaekasui <kaekasui_github@icloud.com>
2018-01-04 16:39:00 +0900

    Add the part of equipages

--- frontend/src/javascripts/equipage_ability/equipage_setting.js
+++ frontend/src/javascripts/equipage_ability/equipage_setting.js
@@ -13,11 +13,17 @@ export default class EquipageSetting extends React.Component {
       <div className='equipageSettingComponent'>
         <Title title='◆装備' />
         <div className='equipages'>
+          <span>{'手'}</span>
           <Equipage part='hand' />
+          <span>{'頭'}</span>
           <Equipage part='head' />
+          <span>{'体'}</span>
           <Equipage part='body' />
+          <span>{'足'}</span>
           <Equipage part='foot' />
+          <span>{'アクセ'}</span>
           <Equipage part='accessory' />
+          <span>{'アクセ'}</span>
           <Equipage part='accessory' />
         </div>
       </div>
