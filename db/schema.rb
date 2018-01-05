2b782e742f4ec60f8c8288131f2b1ac0f21715d0
kaekasui <kaekasui_github@icloud.com>
2018-01-05 21:20:16 +0900

    Add the column name for the grades

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102140936) do
+ActiveRecord::Schema.define(version: 20180105121713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20180102140936) do
     t.string "image_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
   end
 
   create_table "hero_abilities", force: :cascade do |t|
