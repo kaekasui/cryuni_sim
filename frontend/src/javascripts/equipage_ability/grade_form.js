b1fc4e9e86150622e127e21f2e6f90573cbf932c
kaekasui <kaekasui_github@icloud.com>
2018-01-05 21:56:20 +0900

    Display the grades buttons

---
+++ frontend/src/javascripts/equipage_ability/grade_form.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class GradeForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedGradeLevel: undefined
+    }
+    this.handleChangeGradeLevel = this.handleChangeGradeLevel.bind(this)
+  }
+
+  handleChangeGradeLevel(e) {
+    this.setState({selectedGradeLevel: e.target.value})
+  }
+
+  render() {
+    return (
+      <div className='gradeFormComponent'>
+        <div className='btn-group' data-toggle='buttons'>
+          {this.props.grades.map((grade) => (
+            <label className={'btn btn-default ' + (this.state.selectedGradeLevel == grade.level ? 'active' : '')} id={'level-' + grade.level} key={grade.id}>
+              <input autoComplete='off' name='grade-level' onChange={this.handleChangeGradeLevel} type='radio' value={grade.level} />
+              {grade.name}
+            </label>
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+GradeForm.propTypes = {
+  grades: PropTypes.array.isRequired
+}
