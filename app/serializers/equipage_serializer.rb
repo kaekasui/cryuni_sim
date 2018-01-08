1b5dc55b54ae42bc89c8ddf1301d9b4f1d89d1ad
kaekasui <kaekasui_github@icloud.com>
2018-01-08 16:50:36 +0900

    Add the part human name for equipage

--- app/serializers/equipage_serializer.rb
+++ app/serializers/equipage_serializer.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class EquipageSerializer < ActiveModel::Serializer
-  attributes :id, :name, :part, :level, :min_grade, :max_grade,
+  attributes :id, :name, :part, :part_human_name, :level, :min_grade, :max_grade,
              :card_slot, :image_name, :range_grades
 
   def range_grades
     object.range_grades.map { |r| GradeSerializer.new(r) }
+  end
+
+  def part_human_name
+    I18n.t("labels.part.#{object.part}")
   end
 end
