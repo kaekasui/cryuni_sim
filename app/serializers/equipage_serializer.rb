f691f09ba6d92ca7fb0c67ceed1582829ae0b30b
kaekasui <kaekasui_github@icloud.com>
2018-01-08 18:19:42 +0900

    Fix following rubocop rules

--- app/serializers/equipage_serializer.rb
+++ app/serializers/equipage_serializer.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class EquipageSerializer < ActiveModel::Serializer
-  attributes :id, :name, :part, :part_human_name, :level, :min_grade, :max_grade,
-             :card_slot, :image_name, :range_grades
+  attributes :id, :name, :part, :part_human_name, :level,
+             :min_grade, :max_grade, :card_slot, :image_name, :range_grades
 
   def range_grades
     object.range_grades.map { |r| GradeSerializer.new(r) }
