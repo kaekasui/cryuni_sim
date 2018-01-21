b8b2bc5835822242ac318676ae9d073b77ade114
kaekasui <kaekasui_github@icloud.com>
2018-01-21 17:18:10 +0900

    Remove commentout

--- frontend/src/javascripts/hero_ability/hero_abilities.js
+++ frontend/src/javascripts/hero_ability/hero_abilities.js
@@ -9,27 +9,6 @@ export default class HeroAbilities extends React.Component {
     this.handleClickHeroAbility = this.handleClickHeroAbility.bind(this)
   }
 
-  //shouldComponentUpdate(nextProps) {
-  //  if (nextProps.intimacy !== this.props.intimacy || nextProps.heroAbilities !== this.props.heroAbilities) {
-  //    return true
-  //  } else {
-  //    return false
-  //  }
-  //}
-
-  //componentDidUpdate() {
-  //  let updated = false
-  //  for (let index in this.props.heroAbilities) {
-  //    if (this.props.heroAbilities[index].intimacy_level_from <= this.props.intimacy && this.props.intimacy <= this.props.heroAbilities[index].intimacy_level_to) {
-  //      this.props.handleSelectHeroAbility(this.props.heroAbilities[index])
-  //      updated = true
-  //    }
-  //  }
-  //  if (updated == false) {
-  //    this.props.handleSelectHeroAbility({attached_hero_abilities: []})
-  //  }
-  //}
-  //
   handleClickHeroAbility(ability) {
     this.props.handleSelectHeroAbility({attached_hero_abilities: ability.attached_hero_abilities}, ability.id)
   }
