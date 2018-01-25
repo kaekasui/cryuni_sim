17dcbb6016a222bc08bc3b0cb52558794875fbd7
kaekasui <kaekasui_github@icloud.com>
2018-01-25 20:12:32 +0900

    Display the attack icons on image

--- frontend/src/javascripts/results/attack.js
+++ frontend/src/javascripts/results/attack.js
@@ -11,6 +11,7 @@ export default class Attack extends React.Component {
       <div className='attackComponent'>
         <span className='abilityName'>{this.props.abilityName}</span>
         <br />
+        <img className='ability-icon' src='/assets/ability_icons/attack_icon.png' />
         <span>{'+' + this.props.score}</span>
         <span>{'%'}</span>
         <span className='attack-shadow' />
