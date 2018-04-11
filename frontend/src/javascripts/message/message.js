0f9ee28842d0625835684c58acadba9770d91a8f
kaekasui <kaekasui_github@icloud.com>
2018-04-12 05:04:42 +0900

    Add the button to open modal for twitter

--- frontend/src/javascripts/message/message.js
+++ frontend/src/javascripts/message/message.js
@@ -1,13 +1,28 @@
 import React from 'react'
 
+import ModalTwitter from './modal_twitter'
+
 export default class Message extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      modalIsOpen: false
+    }
     this.handleClickPanel = this.handleClickPanel.bind(this)
+    this.handleClickInfoButton = this.handleClickInfoButton.bind(this)
+    this.onClickCloseButton = this.onClickCloseButton.bind(this)
   }
 
   handleClickPanel() {
     window.open('http://amzn.to/2D4YEfG')
+  }
+
+  handleClickInfoButton() {
+    this.setState({modalIsOpen: true})
+  }
+
+  onClickCloseButton() {
+    this.setState({modalIsOpen: false})
   }
 
   render() {
@@ -17,6 +32,10 @@ export default class Message extends React.Component {
           <div className='panel-body message'>
             {'一日一回クリックを何卒ーヾ(｡>﹏<｡)ﾉﾞ✧*。'}
           </div>
+        </div>
+        <div>
+          <a className='btn btn-default' onClick={this.handleClickInfoButton}>{'お知らせ'}</a>
+          <ModalTwitter handleClickCloseButton={this.onClickCloseButton} modalIsOpen={this.state.modalIsOpen} />
         </div>
       </div>
     )
---
+++ frontend/src/javascripts/message/modal_twitter.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Modal from 'react-modal'
+
+export default class ModalTwitter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleClickCloseButton = this.handleClickCloseButton.bind(this)
+  }
+
+  handleClickCloseButton() {
+    this.props.handleClickCloseButton()
+  }
+
+  render() {
+    return (
+      <div className='modalTwitterComponent'>
+        <Modal ariaHideApp={false} isOpen={this.props.modalIsOpen} onRequestClose={this.handleClickCloseButton}>
+          <button aria-label='Close' className='close' onClick={this.handleClickCloseButton} type='button'>
+            <span dangerouslySetInnerHTML={{__html: '&times'}} />
+          </button>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+ModalTwitter.propTypes = {
+  handleClickCloseButton: PropTypes.func.isRequired,
+  modalIsOpen: PropTypes.bool.isRequired
+}
