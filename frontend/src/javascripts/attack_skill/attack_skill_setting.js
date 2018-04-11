d93d04968333f18367f4ae1eac82110bb82ac010
kaekasui <kaekasui_github@icloud.com>
2018-04-12 02:06:18 +0900

    Add the commint soon titles

---
+++ frontend/src/javascripts/attack_skill/attack_skill_setting.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Title from './../common/title'
+
+export default class AttackSkillSetting extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className='attackSkillSettingComponent'>
+        <Title title='◆英雄の討伐スキル' />
+        {'Coming Soon!!'}
+      </div>
+    )
+  }
+}
