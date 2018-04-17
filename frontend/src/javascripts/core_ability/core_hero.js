1f4fe50eb1f51cdb1e9dd874fd94c0dc8cd08d6d
kaekasui <kaekasui_github@icloud.com>
2018-04-18 01:10:50 +0900

    Add hero icons and Display no image when no image

--- frontend/src/javascripts/core_ability/core_hero.js
+++ frontend/src/javascripts/core_ability/core_hero.js
@@ -13,11 +13,16 @@ export default class CoreHero extends React.Component {
   }
 
   render() {
+    let image_path = this.props.hero.image_name ? (
+      'assets/hero_icons/' + this.props.hero.image_name
+    ) : (
+      'assets/hero_icons/no_image_icon.jpg'
+    )
     return (
       <div className='coreHeroComponent'>
         <span className='hero-image' data-tip={this.props.hero.name} >
           <ReactTooltip />
-          <img alt={this.props.hero.name} className={this.props.hero.padlocked ? 'locked' : ''} onClick={this.handleClickCoreHeroImage} src={'assets/hero_icons/' + this.props.hero.image_name} />
+          <img alt={this.props.hero.name} className={this.props.hero.padlocked ? 'locked' : ''} onClick={this.handleClickCoreHeroImage} src={image_path} />
           { this.props.hero.padlocked ? (
             <img className='padlock' onClick={this.handleClickCoreHeroImage} src='assets/padlock.png' />
           ) : (
