afe812f59e122674f2fa60fffacc5a8a0ef032a9
kaekasui <kaekasui_github@icloud.com>
2018-04-27 23:37:50 +0900

    Design for the twitter modal

--- frontend/src/javascripts/equipage_ability/modal/modal_equipages_list.js
+++ frontend/src/javascripts/equipage_ability/modal/modal_equipages_list.js
@@ -61,37 +61,39 @@ export default class ModalEquipagesList extends React.Component {
     return (
       <div className='modalEquipagesListComponent'>
         <Modal ariaHideApp={false} isOpen={this.props.modalIsOpen}>
-          <button aria-label='Close' className='close' onClick={this.handleClickCloseButton} type='button'>
-            <span dangerouslySetInnerHTML={{__html: '&times'}} />
-          </button>
-          {this.state.isLoading ? (
-            <Loading />
-          ) : (
-            <div className='equipages-list'>
-              <table className='table table-bordered'>
-                <tbody>
-                  <tr className='modal-equipage-line' onMouseEnter={this.handleMouseLeaveEquipage}>
-                    <td colSpan='2'>
-                      <span>{'なし'}</span>
-                      <img className='equipage-button remove-equipage' onClick={this.handleClickEmptyEquipage} src='assets/equipages/remove_equipage.png' />
-                    </td>
-                  </tr>
-                  {this.state.equipages.map((equipage) =>
-                    <ModalEquipage equipage={equipage} key={equipage.id} mouseEnterEquipage={this.state.mouseEnterEquipage} onClickEquipage={this.handleClickEquipage} onMouseEnterEquipage={this.handleMouseEnterEquipage} />
-                  )}
-                </tbody>
-              </table>
-              {Object.keys(this.state.mouseEnterEquipage).length > 0 ? (
-                <div className='panel panel-default'>
-                  <div className='panel-body'>
-                    <EquipageInformation equipage={this.state.mouseEnterEquipage} />
+          <div className='equipagesListModal'>
+            <button aria-label='Close' className='close' onClick={this.handleClickCloseButton} type='button'>
+              <span dangerouslySetInnerHTML={{__html: '&times'}} />
+            </button>
+            {this.state.isLoading ? (
+              <Loading />
+            ) : (
+              <div className='equipages-list'>
+                <table className='table table-bordered'>
+                  <tbody>
+                    <tr className='modal-equipage-line' onMouseEnter={this.handleMouseLeaveEquipage}>
+                      <td colSpan='2'>
+                        <span>{'なし'}</span>
+                        <img className='equipage-button remove-equipage' onClick={this.handleClickEmptyEquipage} src='assets/equipages/remove_equipage.png' />
+                      </td>
+                    </tr>
+                    {this.state.equipages.map((equipage) =>
+                      <ModalEquipage equipage={equipage} key={equipage.id} mouseEnterEquipage={this.state.mouseEnterEquipage} onClickEquipage={this.handleClickEquipage} onMouseEnterEquipage={this.handleMouseEnterEquipage} />
+                    )}
+                  </tbody>
+                </table>
+                {Object.keys(this.state.mouseEnterEquipage).length > 0 ? (
+                  <div className='panel panel-default'>
+                    <div className='panel-body'>
+                      <EquipageInformation equipage={this.state.mouseEnterEquipage} />
+                    </div>
                   </div>
-                </div>
-              ) : (
-                null
-              )}
-            </div>
-          )}
+                ) : (
+                  null
+                )}
+              </div>
+            )}
+          </div>
         </Modal>
       </div>
     )
